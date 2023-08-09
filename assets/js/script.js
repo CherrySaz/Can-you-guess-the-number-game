8d7acc14c99ff2f591e65342c41eec9b5b5f2425
CherrySaz <>
2023-08-09 20:11:10 +0000

    add eventlisteners and message to user on initial play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-console.log('connected!')
+let randomNumber = Math.floor(Math.random() * 500) + 1;
+let attempts = 0;
+
+let guessInput = getElementById('guessInput');
+let checkanswerButton = getElementById('checkanswerButton');
+let messagetouser = getElementById('messagetouser');
+
+checkanswerButton.addEventListener('click', ()=> 0 {
+
+    let usersGuess = parseInt (guessInput.value);
+})
+
+if isNaN(usersGuess) || usersGuess < 1 || usersGuess > 500 {
+    messagetouser = 'Feeling lucky? Guess a number between 1 and 500';
+    messagetouser.style.color.'#941631';
+    return;
+}
+
+userattempts++;
+
+if (usersGuess === randomNumber) {
+    messagetouser.textcontent
+}
+
